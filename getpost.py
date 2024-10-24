59dd0df1cb2ff7a045f78ce242ed59e8e09c2a2f
Admin <sharafunneesa111.pp@gmail.com>
2024-10-24 05:55:49 +0400

    getpost updated

--- getpost.py
+++ getpost.py
@@ -11,9 +11,9 @@ app=Flask(__name__)
 def welcome():
     return "<html><h1>Wecome to flask learning</h1></html>"
 
-@app.route("/index",methods=["GET"])
-def index():
-    return render_template("index.html")
+# @app.route("/index",methods=["GET"])
+# def index():
+#     return render_template("index.html")
 
 @app.route("/about")
 def about():
