29272ed9f2155504e0ee2970bf8ebe759daf5792
Admin <sharafunneesa111.pp@gmail.com>
2024-10-29 01:11:48 +0400

    variable rule, building dynamic url

--- jinga.py
+++ jinga.py
@@ -1,6 +1,6 @@
 #building url dynamically
 #variable rule
-#jinga 2 template engine
+#jinga 2 template engine- any information get from form  can passed 
 
 
 
@@ -13,10 +13,7 @@
 """
 
 
-
-
-
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 # initialise 
 """
 it will create an instance of the flask class.
@@ -33,12 +30,15 @@ def submit():
         return f"hello {name}!"
     return render_template("form.html")
 
+
 # # variable rule
 # @app.route("/success/<int:score>") #assigning a rule to variable
 # def success(score):
 #     return f"The marks you got is {score}" # do typecasting
 
 
+
+# passing value in this  and giving restrictions
 @app.route("/success/<int:score>") #assigning a rule to variable
 def success(score):
     res=""
@@ -62,13 +62,29 @@ def successres(score):
     return render_template('result1.html',results=res)
 
 
-@app.route("/successif/<int:score>") #assigning a rule to variable
-def successif(score):
+@app.route("/fail/<int:score>") #assigning a rule to variable
+def fail(score):
 
     return render_template('result.html',results=score)
 
+@app.route('/submits',methods=['POST','GET'])
+def submits():
+    total_score=0
+    if request.method=='POST':
+        science=float(request.form["science"])
+        maths=float(request.form["maths"])
+        total_score=(science+maths)/2
+    else:
+        return render_template('getresult.html')
+    return redirect(url_for('successres',score=total_score)) # building dynamic url
 
-    
+
+#if condition
+@app.route("/successif/<int:score>") #assigning a rule to variable
+def successif(score):
+    res=""
+
+    return render_template('result.html',results=score)
 
 
 # this is the entry point of any .py file
